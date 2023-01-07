869d2bf27a05c8fbfdb68c5be4ed936e2b4601f9
Gustavo Mafra <mafraguh@gmail.com>
2023-01-07 20:46:46 -0300

    Added carousel component

---
+++ src/Components/Carousel.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import Card from './Card';
+import '../Styles/Carousel.scss';
+import { GrPrevious, GrNext } from 'react-icons/gr';
+import StartFirebase from './StartFirebase';
+import { ref, get } from 'firebase/database';
+import { Loading } from 'react-loading-dot';
+
+
+function Carousel() {
+
+    const [cards, setCards] = useState(false);
+
+    useEffect(() => {
+        const db = StartFirebase();
+        const dbRef = ref(db)
+        get(dbRef, 'Products/').then((response) => setCards(response.val()))
+    }, [])
+
+    return cards ? (
+        <div className='CarouselContainer'>
+            <div className='CarouselController'><GrPrevious size={60} /></div>
+            <div className='CarouselCards'>
+                <div className='CarouselCardsContainer'>
+                    {
+                        Object.keys(cards.Products).map((card, key) => {
+                            return (
+                                <Card content={cards.Products[card]} name={card} key={key} />
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <div className='CarouselController'><GrNext size={60} /></div>
+        </div>
+    ) :  <Loading background='Black'/>
+}
+
+export default Carousel;
