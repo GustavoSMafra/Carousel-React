af2b45eafb220aa67a21e071a6a3246230b6935b
Gustavo Mafra <mafraguh@gmail.com>
2023-01-08 18:32:44 -0300

    Added handle animation

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import Card from './Card';
 import '../Styles/Carousel.scss';
-import { GrPrevious, GrNext } from 'react-icons/gr';
 import StartFirebase from './StartFirebase';
 import { ref, get } from 'firebase/database';
 import { Loading } from 'react-loading-dot';
@@ -18,11 +17,17 @@ function Carousel() {
         get(dbRef, 'Products/').then((response) => setCards(response.val()))
     }, [])
 
+    const handleAnimation = () => {
+        setAnimation(!animation)
+        setTimeout(()=> {
+            setAnimation(true)
+        }, 10000)
+    }
+
     return cards ? (
         <div className='CarouselContainer'>
-            <div className='CarouselController'><GrPrevious size={60} onClick={() => setAnimation(false)}/></div>
             <div className='CarouselCards'>
-                <div className={`CarouselCardsContainer ${animation}`}>
+                <div className= {`CarouselCardsContainer ${animation}`} onClick={handleAnimation}>
                     {
                         Object.keys(cards.Products).map((card, key) => {
                             return (
@@ -32,7 +37,6 @@ function Carousel() {
                     }
                 </div>
             </div>
-            <div className='CarouselController'><GrNext size={60} onClick={() => setAnimation(false)} /></div>
         </div>
     ) :  <Loading background='Black'/>
 }
