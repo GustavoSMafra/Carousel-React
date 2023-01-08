a7b827248ef39ec42cbfe6d212b7dabe29d4e3cd
Gustavo Mafra <mafraguh@gmail.com>
2023-01-08 13:26:17 -0300

    Added card style and information

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react';
-import '../Styles/Card.scss'
+import { Button } from '@mui/material';
+import '../Styles/Card.scss';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 
 function Card(props) {
     const [card] = useState(props.content)
     const [name] = useState(props.name)
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+    const startFunction = (rate) => {
+        let elements = []
+        for (let i = 1; i <= 5; i++) {
+            elements.push(
+                rate >= i ? rate >= i + 1 ? <BsStarFill key={i}/> :
+                    <BsStarHalf key={i}/> : <BsStar key={i}/>
+            )
+        }
+        return elements
+    }
     return (
         <div className='CardContainer'>
-            <div>
-                <h1>{name}</h1>
-                <p>{card.Value}</p>
+            <div className='CardContainerItem'>
+                <div className='CardContainerImg'><img src={card.Src} alt="" /></div>
+                <div className='CardDescription'>
+                    <h1>{name}</h1>
+                    <p>Price: {formatter.format(card.Value)}</p>
+                    <div className='CardRate'>
+                        <p>Rate:</p>
+                        {startFunction(card.Rate)}
+                    </div>
+                </div>
+                <div className='CardButton'>
+                    <Button>Add</Button>
+                </div>
             </div>
         </div>
     );
